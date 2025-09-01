83d482b48a6aea3d2294040f4bfbc93c60003358
Ryan G <exogenesis@protonmail.com>
2025-09-01 12:47:51 +1000

    reduce exports

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 // Export all Layout components
-export * from './components/Layout';
+// export * from './components/Layout';
 
 // // Export all Combobox components
 // export * from './components/Combobox';
 
 // Export utility components
 export { VirtualList } from './components/VirtualList';
-export { VirtualPhotoGrid } from './components/VirtualPhotoGrid';
+// export { VirtualPhotoGrid } from './components/VirtualPhotoGrid';
 
 // Export presets and themes
 export * from './presets/daisy/daisy';
