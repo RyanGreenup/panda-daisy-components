971682b75de9c9b848c21a233828f51444f1bfb9
Ryan G <exogenesis@protonmail.com>
2025-09-06 17:37:44 +1000

    update package.json

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -11,6 +11,23 @@ export default defineConfig([
     // dom is required for client side
     // I CANNOT get it to work for both nor can I figure this out
     // I don't think it's worth the effort when git submodules work painlessly
+    //
+    // The [README](./node_modules/rolldown-plugin-solid/README.md) suggest this:
+    //
+    // ```ts`
+    // plugins: [solid({ solid: {
+    //     generate: 'universal',
+    //     moduleName: 'solid-js/universal'
+    // } })],
+    // ````
+    //
+    // but it fails with:
+    //
+    //````
+    // node:internal/fs/watchers:254
+    //     const error = new UVException({
+    // ````
+    // I've spent a week on this, I think I'm just going to use git submodules
     plugins: [solid({ solid: { generate: "dom" } })],
   },
   {
